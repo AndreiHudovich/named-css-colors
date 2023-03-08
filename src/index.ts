b1cc68d54a385d55d6c38a7b2550235583bcbf92
Andrei Hudovich <andrei.hudovich@gmail.com>
2023-03-08 19:54:18 +0100

    feat: add basic utils

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import {
+  namedColors,
+  namedColorsIdentifiers,
+  namedColorsHexs
+} from './colors.js'
+import { isNamedColor } from './utils.js'
+import type { NamedColor } from './types'
+
+export { namedColors, namedColorsIdentifiers, namedColorsHexs, isNamedColor }
+export type { NamedColor }
